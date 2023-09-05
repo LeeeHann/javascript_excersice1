e7e465da2edc9e868379882710b2f7306cf2b44f
lehan <han@baytech.co.jp>
2023-09-05 15:09:56 +0900

    excercise 2_bài 10 đang suy nghĩ lại

--- excercise1.js
+++ excercise1.js
@@ -1,64 +1,64 @@
-//1. Write function to check if number is even, isEven(2) -> true, isEven(3) -> false
+// //1. Write function to check if number is even, isEven(2) -> true, isEven(3) -> false
 
-function isEven(number) {
-    if (number % 2 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  console.log(isEven(3));
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+//   console.log(isEven(3));
 
-//2. Write function to convert string to prefix string, example: prefixStr('i love to learn','-') -> 'i-love-to-learn'
+// //2. Write function to convert string to prefix string, example: prefixStr('i love to learn','-') -> 'i-love-to-learn'
 
-// regular expression
-function prefixStr(d) {
-    const prefixStr = d.replace(/ /g,"-");
-    return prefixStr;
-  }
-console.log(prefixStr(`i love to learn`));
+// // regular expression
+// function prefixStr(d) {
+//     const prefixStr = d.replace(/ /g,"-");
+//     return prefixStr;
+//   }
+// console.log(prefixStr(`i love to learn`));
 
-//3. Write function to calculate age, example: calculateAge(1994) -> 29
+// //3. Write function to calculate age, example: calculateAge(1994) -> 29
 
-function birthyear(birth){
-    const age = 2023 - birth;
-    return age;
-}
-console.log(birthyear(2000))
+// function birthyear(birth){
+//     const age = 2023 - birth;
+//     return age;
+// }
+// console.log(birthyear(2000))
 
-//4. Write function sum of three numbers, fn(a,b,c)
+// //4. Write function sum of three numbers, fn(a,b,c)
 
-    function sum(a,b,c) {
-        const total=a+b+c;
-        return total;
-    }
-    console.log(sum(1,2,3));
+//     function sum(a,b,c) {
+//         const total=a+b+c;
+//         return total;
+//     }
+//     console.log(sum(1,2,3));
 
-//5. Write function to get random number from min to max
+// //5. Write function to get random number from min to max
 
-//math.floor
-//math.random
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandomNumber(1, 5)); 
+// //math.floor
+// //math.random
+// function getRandomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(getRandomNumber(1, 5)); 
 
-//6. Write function return typeof value, checkTypeOf('evondev') -> string
+// //6. Write function return typeof value, checkTypeOf('evondev') -> string
 
-// （typeof value）関数はタイプをチェックする
-function check(a) {
-return typeof a;
-}
-console.log(check(`evondev`));
+// // （typeof value）関数はタイプをチェックする
+// function check(a) {
+// return typeof a;
+// }
+// console.log(check(`evondev`));
 
-//7. Write function to check if str includes word, isIncludes("evondev", "vonz") -> false
-//a.includes(b): ｂのa
+// //7. Write function to check if str includes word, isIncludes("evondev", "vonz") -> false
+// //a.includes(b): ｂのa
 
-function isIncludes(str, word) {
-    return str.includes(word);
-  }
-console.log(isIncludes("evondev", "von")); // true
-// // console.log(isIncludes("evondev", "vonz")); //false
+// function isIncludes(str, word) {
+//     return str.includes(word);
+//   }
+// console.log(isIncludes("evondev", "von")); // true
+// // // console.log(isIncludes("evondev", "vonz")); //false
 
 
 
---
+++ excercise2.js
@@ -0,0 +1,212 @@
+// Viết 1 function để tạo ra 1 mảng với với số lượng và chuỗi truyền vào. Ví dụ repeatValueArr(3, 'a') trả ra kết quả là mảng như sau: ['a','a','a']
+
+function repeatValueArr(count, value) {
+    let result = [];
+    for (
+        let i = 0; 
+        i < count; 
+        i++) 
+    {     
+    result.push(value); 
+    }
+    return result;
+}
+
+console.log(repeatValueArr(3, 'a')); 
+
+// Viết 1 function để đảo ngược mảng truyền vào. Ví dụ reverseArr([1,2,3,4,5]) sẽ có kết quả là [5,4,3,2,1]
+
+function reverseArr(arr) {  
+    let result1 = [];  
+    for (
+        let i = arr.length - 1; 
+        i >= 0; 
+        i--) 
+    {    
+        result1.push(arr[i]);  
+    }  
+    return result1;
+}
+
+console.log(reverseArr([1,2,3,4,5]));
+
+
+// Viết 1 function để loại ra các phần tử là Falsy Values trong mảng. Ví dụ removeFalsyArr([0, 1, false, 2, undefined, '', 3, null]) kết quả trả về sẽ là [1,2,3]
+
+function removeFalsyArr(arr) {  
+    let result2 = [];  
+    for (
+        let i = 0; 
+        i < arr.length; 
+        i++) 
+    {   
+        if (arr[i]) 
+        {
+            result2.push([i]);    
+        }  
+    }  
+    return result2;
+}
+
+console.log(remove([0, 1, false, 2, undefined, '', 3, null])); 
+
+
+// Viết 1 function để loại ra các phần tử trùng nhau ở trong mảng. Ví dụ removeDuplicate([1,2,3,1,2,3,4,5]) sẽ có kết quả là [1,2,3,4,5]
+
+function removeDuplicate(arr){ 
+    let result3=[];
+    for (
+        let i=0; 
+        i<arr.length;
+        i++
+        )
+    {
+        if (result3.indexOf(arr[i]) === -1)
+        {
+            result3.push(arr[i]);
+        }
+    }
+    return result3;
+
+}
+
+console.log(removeDuplicate([1,2,3,1,2,3,4,5]));
+
+
+
+// Viết 1 function để tạo ra 1 mảng mới chứa nhiều mảng con theo số lượng truyền vào. Ví dụ chunkArr([1, 2, 3, 4, 5], 2) sẽ có kết quả trả về là [[1, 2], [3, 4], [5]]
+
+function chunkArr(arr, size) {  
+    let result4 = [];
+    for (
+        let i = 0; 
+        i < arr.length; 
+        i += size
+        ) 
+    {    
+        result4.push(arr.slice(i, i + e));  
+    }  
+    return result4;
+}
+let arr = [1, 2, 3, 4, 5];
+let size = 2;
+console.log(chunkArr(d, e));
+
+// Viết 1 function chỉ trả ra các số là số chẵn từ mảng. Ví dụ isEvenArr([13,23,12,45,22,48,66,100]) -> [12,22,48,66,100]
+function isEvenArr(arr) {  
+    let result5 = [];  
+    for (
+        let i = 0; 
+        i < arr.length; 
+        i++
+        ) 
+    {    
+        if (arr[i] % 2 === 0) 
+            {      
+                result5.push(arr[i]);  
+            }  
+    }  
+    return result5;
+}
+
+console.log(isEvenArr([13,23,12,45,22,48,66,100])); 
+
+// Viết 1 function tìm ra số lớn nhất trong mảng. Ví dụ largestNumber([2, 45, 3, 67, 34, 567, 34, 345, 123]) kết quả là 567
+
+function largestNumber(arr) {  
+    return Math.max(...arr);
+}
+console.log(largestNumber([2, 45, 3, 67, 34, 567, 34, 345, 123]));
+
+// // Viết 1 function có tên là capitalize(str) để biến tất cả chữ thành IN HOA. Ví dụ: capitalize("evondev") có kết quả là EVONDEV
+function capitalize(str) {  
+    return str.toUpperCase();
+}
+console.log(capitalize("evondev"));
+
+// Cho 1 mảng đơn giản như sau
+const people = [
+  { name: "Charlie", age: 28, city: "Chicago" },
+  { name: "Alice", age: 25, city: "New York" },
+  { name: "Bob", age: 32, city: "Los Angeles" },
+];
+// 9.1 Viết 1 hàm có tên là filterByAge(people, age) để trả về 1 mảng những người có độ tuổi lớn hơn 1 giá trị nhất định
+
+// function filterByAge(people, age) {
+//       return people.filter(person => person.age > age);
+//     }
+// console.log(filterByAge(people, 30));
+
+// 9.2 Viết 1 hàm có tên là sortByNames(people) để sắp xếp tên người theo thứ tự tăng dần 
+function sortByNames(people) {
+    return people.sort((a, b) => {
+??
+ ??     if (a < b) {
+            return -1; 
+        }
+        if (a > b) {
+            return 1;
+        }
+        return 0;
+    });
+}
+
+console.log(sortByNames(people));
+
+// 9.3 Viết 1 hàm có tên là findPeopleByCity(people, city) để trả ra đối tượng thuộc thành phố nào đó 
+function findPeopleByCity(people, city) {
+    let result = [];  
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].city === city) {
+            result.push(people[i]);    
+        }  
+    }  
+    return result;
+}
+// 9.4 Viết 1 hàm có tên là deletePeopleById(people, id) để xóa 1 người nào đó trong mảng people
+function deletePeopleById(people, id) {  
+    for (
+        let i = 0; 
+        i < people.length; 
+        i++
+        ) 
+        {
+            if (people[i].id === id) {
+                people.splice(i, 1);
+                return;    
+            }  
+        }
+    }
+    console.log(deletePeopleById(people, 1));
+
+// 9.5 Viết 1 hàm có tên là getAverageAge(people) để tính ra số tuổi trung bình 
+function getAverageAge(people) {
+    let totalAge = 0;  
+    for (
+        let i = 0; 
+        i < people.length; 
+        i++) 
+        {    
+            totalAge += people[i].age; //tính tổng tuổi của mảng
+        }  
+        return totalAge / people.length;
+    }
+
+    console.log(getAverageAge(people));
+
+// 10(Khó). Viết 1 hàm có tên là countLetters(str) để đếm các kí tự trong chuỗi và trả ra 1 object chứa các từ đó. 
+// Ví dụ
+// countLetters('abbcccddddeeeee');
+// {a: 1, b: 2, c: 3, d: 4, e: 5}
+// Gợi ý biến chuỗi thành mảng sử dụng [...str]
+// Gợi ý sử dụng hasOwnProperty trong Object và phương thức reduce trong mảng (đang suy nghĩ)
+function countLetters(str) {  
+    const letters = [...str];
+    const count = {};
+    for (
+        let i = 0; 
+        i < letters.length; 
+        i++
+        )
+        const letter = letters[i];//
+        //?
